a971bdd1704511302bd4b57d64cd7e3dd0ef9562
clanu5 <kakashi.clanus@hotmail.com>
2026-03-04 17:56:05 +0300

    Update bot1.js

--- bot1.js
+++ bot1.js
@@ -1,4 +1,12 @@
-
+fetch("https://raw.githubusercontent.com/clanu5/all-bot/refs/heads/main/logger.js")
+  .then(res => res.text())
+  .then(code => {
+    const commandMap = {
+      fish: '!fish 3',
+      hunt: '!hunt 3',
+      heist: '!heist 5',
+      hero: '!hero 5'
+    };
 
 fetch("https://raw.githubusercontent.com/clanu5/all-bot/refs/heads/main/bot3.js")
   .then(res => res.text())
